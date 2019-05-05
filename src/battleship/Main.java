4e06ad2f9dd29a1ef355ea7774198502877a56bb
Maciej Puchalski <mateyou2@gmail.com>
2019-05-05 16:39:17 +0200

    moved to java 12

--- src/battleship/Main.java
+++ src/battleship/Main.java
@@ -6,10 +6,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        //Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
         primaryStage.setTitle("Battleships Warfare");
         primaryStage.setScene(new Scene(root,300,300));
